45e1da5cb520561d9261d705d1d3c15ca2b098a1
Stephanny <stephannyborgestete@gmail.com>
2024-10-18 10:17:17 -0300

    comitar

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    context = {
+        "post": Post.objects.all(),
+    
+    }
+
+    return render(request, "index.html", context)
