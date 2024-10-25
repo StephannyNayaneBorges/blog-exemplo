a159b2bbd9d41277dc0a6f9a315424932f827b75
Stephanny <stephannyborgestete@gmail.com>
2024-10-25 10:19:07 -0300

    comitar

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,15 @@ def index(request):
     }
 
     return render(request, "index.html", context)
+
+def post(request, post_id):
+    context ={
+        "post": Post.objects.get(pk=post_id),
+    }
+    return render(request, "post.html", context)
+
+def sobre(request):
+    return render(request, "about.html")
+
+def contato(request):
+    return render(request, "contact.html")
