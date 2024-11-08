d4380bb9fb1d465e9e78a29fa108651a95d5392b
Stephanny <stephannyborgestete@gmail.com>
2024-11-08 10:10:51 -0300

    comitar

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,39 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Blog, Mensagem
 
 def index(request):
     context = {
         "post": Post.objects.all(),
-    
+        "blog": Blog.objects.first()
     }
-
     return render(request, "index.html", context)
 
 def post(request, post_id):
+
     context ={
         "post": Post.objects.get(pk=post_id),
     }
     return render(request, "post.html", context)
 
+
 def sobre(request):
-    return render(request, "about.html")
+    context = {
+        "blog": Blog.objects.first(),
+    }
+    return render(request, "about.html", context)
 
 def contato(request):
-    return render(request, "contact.html")
+    context = {
+        "blog": Blog.objects.first(),
+    }
+
+    if request.method == "POST":
+        print(request.POST['nome'])
+        print(request.POST['email'])
+        print(request.POST['telefone'])
+        print(request.POST['mensagem'])
+
+        Mensagem.save()
+        return render(request, "contato.html", context)
+    else:
+        return render(request, "contato.html", context)
